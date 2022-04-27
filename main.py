2da9b2cdbd5f0e66569bbfe13181b2784624b1c7
Andrew Sigo <79338172+AndrewSigo@users.noreply.github.com>
2022-04-27 15:21:14 -0400

    Added user input logic  for welcome greeting

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # Mortgage Calculator
 # Final project for CS101
+
+
+def welcome_print(welcome=input("Welcome to the Mortgage Calculator! Do you wish to coninue? Y/N: ")):
+    if welcome == "Y":
+        print("Great! Let us continue.")
+    else:
+        print("That's okay, maybe later.")
+
+
+welcome_print()
