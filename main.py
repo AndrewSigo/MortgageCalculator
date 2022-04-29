275004762ef117535ed99384387f00f3c618afdc
Andrew Sigo <79338172+AndrewSigo@users.noreply.github.com>
2022-04-29 18:46:38 -0400

    Added additional functions
    Added function to get user input for variables used in the calculator, added function to calculate monthly payments, doesn't work yet

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 # Mortgage Calculator
 # Final project for CS101
 
-
-def welcome_print(welcome=input("Welcome to the Mortgage Calculator! Do you wish to coninue? Y/N: ")):
+# welcome meassage asking if user wants to continue
+def welcome_message(welcome=input("Welcome to the Mortgage Calculator! Do you wish to coninue? Y/N: ")):
     if welcome == "Y":
         print("Great! Let us continue.")
     else:
         print("That's okay, maybe later.")
 
 
-welcome_print()
+welcome_message()
+
+# stores user input to variables to be used later to calculate payments
+
+
+def user_info():
+    principle = input("What is the cost of the home?: ")
+    intrest_rate = input("What is the intrest rate?: ")
+    loan_term = input("How many years is the loan?: ")
+    print(principle, loan_term, intrest_rate)
+    # print(principle)
+    # print(intrest_rate)
+    # print(loan_term)
+
+
+# user_info()
+# trying to call user_info and use the variables in the formula
+def payment_calculation():
+    user_info()
+    payment = (principle * intrest_rate) / 1 - \
+        (1 / (1 + intrest_rate) ** loan_term)
+    return f"Your monthly payment is {payment}."
+
+
+print(payment_calculation())
